d0cccaf1ae25e816720c807d89b2e5d107c065e9
basmalahesham <42021079@hti.edu.eg>
2025-07-06 19:37:41 +0300

    pr3,4,5

--- lib/problem5.dart
+++ lib/problem5.dart
@@ -5,12 +5,28 @@
 // You can assume, for the purpose of this kata, that the supplied array will not be empty.
 import 'dart:math';
 import 'package:test/test.dart';
+
 void main() {
   print(findSmallestInt([4, 8, 2, 1]));
   group("Fixed tests", () {
-    test("Test 1", () => expect(findSmallestInt([3,4,5,1,7,2,4,7]), equals(1)));
-    test("Test 2", () => expect(findSmallestInt([324,578,-12,647,34346,12,95,-500]), equals(-500)));
-    test("Test 3", () => expect(findSmallestInt([-346,35467,234,0,254,-39,-999]), equals(-999)));
+    test(
+      "Test 1",
+      () => expect(findSmallestInt([3, 4, 5, 1, 7, 2, 4, 7]), equals(1)),
+    );
+    test(
+      "Test 2",
+      () => expect(
+        findSmallestInt([324, 578, -12, 647, 34346, 12, 95, -500]),
+        equals(-500),
+      ),
+    );
+    test(
+      "Test 3",
+      () => expect(
+        findSmallestInt([-346, 35467, 234, 0, 254, -39, -999]),
+        equals(-999),
+      ),
+    );
   });
 }
 
